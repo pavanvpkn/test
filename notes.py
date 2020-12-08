4aee0fe417c38eee2c700074cc457b7e923b3a6a
pavanvpkn <pavankumarvankalapati18@gmail.com>
2020-12-08 17:58:37 +0530

    first

---
+++ notes.py
@@ -0,0 +1,17 @@
+
+
+#VARIABLES
+x=10#state
+print(x)#10 value stored in the x variable
+print(type(x))#it wil define the type of the function
+print(id(x))
+abc="helo"
+print(abc)
+
+y=30
+
+
+z=50
+
+s=y+z
+print(s)
